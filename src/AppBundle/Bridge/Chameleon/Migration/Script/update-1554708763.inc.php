73ce21d4d66e20e2923b6ff1f5e35e8271057602
Stefan Kruppa <stefan@umulmrum.de>
2019-04-08 10:16:12 +0200

    ref #323: Remove outdated module_cms_search table

---
+++ src/AppBundle/Bridge/Chameleon/Migration/Script/update-1554708763.inc.php
@@ -0,0 +1,8 @@
+<h1>Build #1554708763</h1>
+<h2>Date: 2019-04-08</h2>
+<div class="changelog">
+    - Remove module_cms_search table
+</div>
+<?php
+
+TCMSLogChange::deleteTable('module_cms_search');
